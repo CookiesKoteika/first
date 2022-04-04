249f2b15f863fb2f3a3fb6e4f11364125ccba034
Slava Degelev <demonloord35@gmail.com>
2022-04-04 18:24:17 +0500

    del "printf"

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+printf("%d\n",sum(3,5));
+//	scanf("%d", &c);	
+return s;
+}
+int sum(int a, int b)
+{
+		int s = a+b;
+		return s;
+}
